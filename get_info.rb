b817d4e1bfc5470366f2df1e0ccfc602f50b840b
Umeko Walker <umekolashun@gmail.com>
2020-08-31 22:44:21 +0000

    user input

---
+++ get_info.rb
@@ -0,0 +1,15 @@
+puts "The system needs information about you before we proceed."
+
+puts "Please enter your first and last name"
+
+name = gets.chomp
+
+puts "Thank you! Please enter your age"
+
+age = gets.chomp.to_i 
+
+puts "Please enter your favorite food."
+
+fav_food = gets.chomp
+
+puts name, age.class, fav_food
--- main.rb
+++ main.rb
@@ -1,5 +1,1 @@
 address = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-new_address = address.reverse!
-
-p new_address
